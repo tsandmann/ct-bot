13c8c532ec5b4bebf53587f65018674eb3970e1a
Benjamin Benz <bbe@heise.de>
2006-02-16 00:21:29 +0000

    RC5-Codes ausgelagert und systematisiert

---
+++ include/rc5-codes.h
@@ -0,0 +1,81 @@
+/*
+ * c't-Sim - Robotersimulator fuer den c't-Bot
+ * 
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your
+ * option) any later version. 
+ * This program is distributed in the hope that it will be 
+ * useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+ * PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public 
+ * License along with this program; if not, write to the Free 
+ * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307, USA.
+ * 
+ */
+
+/*! @file 	rc5-codes.h
+ * @brief 	RC5-Fernbedienungscodes
+ * @author 	Andreas Merkle (mail@blue-andi.de)
+ * @date 	15.02.06
+ * Wer diese Datei angepasst hat, kann Sie durch einen .cvsignore Eintrag schützen. 
+ * Dann überschreibt Eclipse Sie nicht mehr automatisch
+*/
+
+#ifndef RC5CODE_H_
+#define RC5CODE_H_
+
+/*!
+ * Fernbedienung mit Jog-Dial-Rad, 
+ * Achtung: Die Adress-Bits muessen auf die Fernbedienung angepasst werden!
+ * Siehe hierzu rc5.c @see RC5_ADDRESS
+ */
+#define noJOG_DIAL
+
+#define RC5_MASK (RC5_COMMAND)	/*!< Welcher Teil des Kommandos wird ausgewertet? */
+
+#define RC5_CODE_0	(0x3940 & RC5_MASK)		/*!< Taste 0 */
+#define RC5_CODE_1	(0x3941 & RC5_MASK)		/*!< Taste 1 */
+#define RC5_CODE_2	(0x3942 & RC5_MASK)		/*!< Taste 2 */
+#define RC5_CODE_3	(0x3943 & RC5_MASK)		/*!< Taste 3 */
+#define RC5_CODE_4	(0x3944 & RC5_MASK)		/*!< Taste 4 */
+#define RC5_CODE_5	(0x3945 & RC5_MASK)		/*!< Taste 5 */
+#define RC5_CODE_6	(0x3946 & RC5_MASK)		/*!< Taste 6 */
+#define RC5_CODE_7	(0x3947 & RC5_MASK)		/*!< Taste 7 */
+#define RC5_CODE_8	(0x3948 & RC5_MASK)		/*!< Taste 8 */
+#define RC5_CODE_9	(0x3949 & RC5_MASK)		/*!< Taste 9 */
+
+#define RC5_CODE_UP	(0x2950 & RC5_MASK)	/*!< Taste Hoch */
+#define RC5_CODE_DOWN	(0x2951 & RC5_MASK)	/*!< Taste Runter */
+#define RC5_CODE_LEFT	(0x2955 & RC5_MASK)	/*!< Taste Links */
+#define RC5_CODE_RIGHT	(0x2956 & RC5_MASK)	/*!< Taste Rechts */
+
+#define RC5_CODE_PWR	(0x394C & RC5_MASK)	/*!< Taste An/Aus */
+
+#ifdef JOG_DIAL		
+	/* Jogdial geht nur inkl. Adresscode */
+	#undef RC5_MASK
+	#define RC5_MASK (RC5_COMMAND | RC5_ADDRESS)
+
+	#define RC5_CODE_JOG_MID	(0x3969 & RC5_MASK)	/*!< Taste Jog-Dial Mitte */
+	#define RC5_CODE_JOG_L1		(0x3962 & RC5_MASK)	/*!< Taste Jog-Dial Links 1 */
+	#define RC5_CODE_JOG_L2		(0x396F & RC5_MASK)	/*!< Taste Jog-Dial Links 2 */
+	#define RC5_CODE_JOG_L3		(0x395F & RC5_MASK)	/*!< Taste Jog-Dial Links 3 */
+	#define RC5_CODE_JOG_L4		(0x3A6C & RC5_MASK)	/*!< Taste Jog-Dial Links 4 */
+	#define RC5_CODE_JOG_L5		(0x3A6B & RC5_MASK)	/*!< Taste Jog-Dial Links 5 */
+	#define RC5_CODE_JOG_L6		(0x396C & RC5_MASK)	/*!< Taste Jog-Dial Links 6 */
+	#define RC5_CODE_JOG_L7		(0x3A6A & RC5_MASK)	/*!< Taste Jog-Dial Links 7 */
+	
+	#define RC5_CODE_JOG_R1		(0x3968 & RC5_MASK)	/*!< Taste Jog-Dial Rechts 1 */
+	#define RC5_CODE_JOG_R2		(0x3975 & RC5_MASK)	/*!< Taste Jog-Dial Rechts 2 */
+	#define RC5_CODE_JOG_R3		(0x396A & RC5_MASK)	/*!< Taste Jog-Dial Rechts 3 */
+	#define RC5_CODE_JOG_R4		(0x3A6D & RC5_MASK)	/*!< Taste Jog-Dial Rechts 4 */
+	#define RC5_CODE_JOG_R5		(0x3A6E & RC5_MASK)	/*!< Taste Jog-Dial Rechts 5 */
+	#define RC5_CODE_JOG_R6		(0x396E & RC5_MASK)	/*!< Taste Jog-Dial Rechts 6 */
+	#define RC5_CODE_JOG_R7		(0x3A6F & RC5_MASK)	/*!< Taste Jog-Dial Rechts 7 */
+#endif	/* JOG_DIAL */
+
+#endif /* RC5CODE_H_ */
--- rc5.c
+++ rc5.c
@@ -19,8 +19,9 @@
 
 /*! @file 	rc5.c
  * @brief 	RC5-Fernbedienung
- * Eventuell muessen die Codes an die jeweilige 
- * Fernbedienung angepasst werden
+ * Um RC5-Codes fuer eine eigene Fernbedienung anzupassen, reicht es diese
+ * in eine Header-Datei auszulagern und anstatt der rc5code.h einzubinden.
+ * Die Maskierung fuer die Auswertung der Codes nicht vergessen!
  * @author 	Benjamin Benz (bbe@heise.de)
  * @date 	20.12.05
 */
@@ -29,176 +30,138 @@
 #include "motor.h"
 #include "bot-logik.h"
 
+#include "rc5-codes.h"
+
+#include <stdlib.h>
+
 #ifdef RC5_AVAILABLE
 
-/*!
- * Fernbedienung mit Jog-Dial-Rad, 
- * Achtung: Die Adress-Bits muessen auf die Fernbedienung angepasst werden!
- */
-//#define JOG_DIAL
-
 #define RC5_TOGGLE		0x0800		/*!< Das RC5-Toggle-Bit */
-#define RC5_ADDRESS		0x07C0		/*!< Der Adressbereich */
-#define RC5_COMMAND		0x103F		/*!< Der Kommandobereich */
+#define RC5_ADDRESS	0x07C0		/*!< Der Adressbereich */
+#define RC5_COMMAND	0x103F		/*!< Der Kommandobereich */
 
-#define RC5_MASK (RC5_COMMAND)		/*!< Welcher Teil des Kommandos wird ausgewertet? */
+/*! 
+ * Dieser Typ definiert Parameter fuer RC5-Kommando-Code Funktionen.
+ */
+typedef struct {
+	uint16 value1;	/*!< Wert 1 */
+	uint16 value2;	/*!< Wert 2 */
+} RemCtrlFuncPar;
 
+/*! Dieser Typ definiert die RC5-Kommando-Code Funktion. */
+typedef void (*RemCtrlFunc)(RemCtrlFuncPar *par);
 
-#define RC5_CODE_0	(0x3940 & RC5_MASK)		/*!< Taste 0 */
-#define RC5_CODE_1	(0x3941 & RC5_MASK)		/*!< Taste 1 */
-#define RC5_CODE_2	(0x3942 & RC5_MASK)		/*!< Taste 2 */
-#define RC5_CODE_3	(0x3943 & RC5_MASK)		/*!< Taste 3 */
-#define RC5_CODE_4	(0x3944 & RC5_MASK)		/*!< Taste 4 */
-#define RC5_CODE_5	(0x3945 & RC5_MASK)		/*!< Taste 5 */
-#define RC5_CODE_6	(0x3946 & RC5_MASK)		/*!< Taste 6 */
-#define RC5_CODE_7	(0x3947 & RC5_MASK)		/*!< Taste 7 */
-#define RC5_CODE_8	(0x3948 & RC5_MASK)		/*!< Taste 8 */
-#define RC5_CODE_9	(0x3949 & RC5_MASK)		/*!< Taste 9 */
+/*! Dieser Typ definiert den RC5-Kommando-Code und die auszufuehrende Funktion. */
+typedef struct {
+	uint16 command;		/*!< Kommando Code */
+	RemCtrlFunc func;	/*!< Auszufuehrende Funktion */
+	RemCtrlFuncPar par;	/*!< Parameter */
+} RemCtrlAction;
 
-#define RC5_CODE_UP	(0x2950 & RC5_MASK)	/*!< Taste Hoch */
-#define RC5_CODE_DOWN	(0x2951 & RC5_MASK)	/*!< Taste Runter */
-#define RC5_CODE_LEFT	(0x2955 & RC5_MASK)	/*!< Taste Links */
-#define RC5_CODE_RIGHT	(0x2956 & RC5_MASK)	/*!< Taste Rechts */
+/*!
+ * Diese Funktion setzt die Geschwindigkeit auf den angegebenen Wert.
+ * @param par Parameter mit den zu setzenden Geschwindigkeiten.
+ */	
+static void rc5_bot_set_speed(RemCtrlFuncPar *par);
 
-#define RC5_CODE_PWR	(0x394C & RC5_MASK)	/*!< Taste An/Aus */
+/*!
+ * Diese Funktion aendert die Geschwindigkeit um den angegebenen Wert.
+ * @param par Parameter mit den relativen Geschwindigkeitsaenderungen.
+ */	
+static void rc5_bot_change_speed(RemCtrlFuncPar *par);
 
-#ifdef JOG_DIAL		
-	// Jogdial geht nur inkl. Adresscode
-	#undef RC5_MASK
-	#define RC5_MASK (RC5_COMMAND |RC5_ADDRESS )
+/*!
+ * Diese Funktion setzt die zu laufenden Encoderschritte.
+ * @param par Parameter mit den zu laufenden Encoderschritten.
+ */	
+static void rc5_bot_goto(RemCtrlFuncPar *par);
 
-	#define RC5_CODE_JOG_MID		(0x3969 & RC5_MASK)	/*!< Taste Jog-Dial Mitte */
-	#define RC5_CODE_JOG_L1		(0x3962 & RC5_MASK)	/*!< Taste Jog-Dial Links 1 */
-	#define RC5_CODE_JOG_L2		(0x396F & RC5_MASK)	/*!< Taste Jog-Dial Links 2 */
-	#define RC5_CODE_JOG_L3		(0x395F & RC5_MASK)	/*!< Taste Jog-Dial Links 3 */
-	#define RC5_CODE_JOG_L4		(0x3A6C & RC5_MASK)	/*!< Taste Jog-Dial Links 4 */
-	#define RC5_CODE_JOG_L5		(0x3A6B & RC5_MASK)	/*!< Taste Jog-Dial Links 5 */
-	#define RC5_CODE_JOG_L6		(0x396C & RC5_MASK)	/*!< Taste Jog-Dial Links 6 */
-	#define RC5_CODE_JOG_L7		(0x3A6A & RC5_MASK)	/*!< Taste Jog-Dial Links 7 */
-	
-	#define RC5_CODE_JOG_R1		(0x3968 & RC5_MASK)	/*!< Taste Jog-Dial Rechts 1 */
-	#define RC5_CODE_JOG_R2		(0x3975 & RC5_MASK)	/*!< Taste Jog-Dial Rechts 2 */
-	#define RC5_CODE_JOG_R3		(0x396A & RC5_MASK)	/*!< Taste Jog-Dial Rechts 3 */
-	#define RC5_CODE_JOG_R4		(0x3A6D & RC5_MASK)	/*!< Taste Jog-Dial Rechts 4 */
-	#define RC5_CODE_JOG_R5		(0x3A6E & RC5_MASK)	/*!< Taste Jog-Dial Rechts 5 */
-	#define RC5_CODE_JOG_R6		(0x396E & RC5_MASK)	/*!< Taste Jog-Dial Rechts 6 */
-	#define RC5_CODE_JOG_R7		(0x3A6F & RC5_MASK)	/*!< Taste Jog-Dial Rechts 7 */
-#endif
+uint16 RC5_Code;	/*!< Letzter empfangener RC5-Code */
 
-uint16 RC5_Code;        /*!< Letzter empfangener RC5-Code */
+/*! Fernbedienungsaktionen */
+static RemCtrlAction gRemCtrlAction[] = {
+	/* RC5-Code,		Funktion,				Parameter */
+	{ RC5_CODE_PWR,		rc5_bot_set_speed,		{ BOT_SPEED_STOP, BOT_SPEED_STOP } },
+	{ RC5_CODE_UP,		rc5_bot_change_speed,	{ 10, 10 } },
+	{ RC5_CODE_DOWN,	rc5_bot_change_speed,	{ -10, -10 } },
+	{ RC5_CODE_LEFT,	rc5_bot_change_speed,	{ 10, 0 } },
+	{ RC5_CODE_RIGHT,	rc5_bot_change_speed,	{ 0, 10 } },
+	{ RC5_CODE_1,		rc5_bot_set_speed,		{ BOT_SPEED_SLOW, BOT_SPEED_SLOW } },
+	{ RC5_CODE_3,		rc5_bot_set_speed,		{ BOT_SPEED_MAX, BOT_SPEED_MAX } },
+	{ RC5_CODE_5,		rc5_bot_goto,			{ 0, 0 } },
+	{ RC5_CODE_6,		rc5_bot_goto,			{ 20, -20 } },
+	{ RC5_CODE_4,		rc5_bot_goto,			{ -20, 20 } },
+	{ RC5_CODE_2,		rc5_bot_goto,			{ 100, 100 } },
+	{ RC5_CODE_8,		rc5_bot_goto,			{ -100, -100 } },
+	{ RC5_CODE_7,		rc5_bot_goto,			{ -40, 40 } },
+	{ RC5_CODE_9,		rc5_bot_goto,			{ 40, -40 } },
+#ifdef JOGDIAL
+	{ RC5_CODE_JOG_MID,	rc5_bot_set_speed,		{ BOT_SPEED_MAX, BOT_SPEED_MAX } },
+	{ RC5_CODE_JOG_L1,	rc5_bot_set_speed,		{ BOT_SPEED_FAST, BOT_SPEED_MAX } },
+	{ RC5_CODE_JOG_L2,	rc5_bot_set_speed,		{ BOT_SPEED_NORMAL, BOT_SPEED_MAX } },
+	{ RC5_CODE_JOG_L3,	rc5_bot_set_speed,		{ BOT_SPEED_SLOW, BOT_SPEED_MAX } },
+	{ RC5_CODE_JOG_L4,	rc5_bot_set_speed,		{ BOT_SPEED_STOP, BOT_SPEED_MAX } },
+	{ RC5_CODE_JOG_L5,	rc5_bot_set_speed,		{ -BOT_SPEED_NORMAL, BOT_SPEED_MAX } },
+	{ RC5_CODE_JOG_L6,	rc5_bot_set_speed,		{ -BOT_SPEED_FAST, BOT_SPEED_MAX } },
+	{ RC5_CODE_JOG_L7,	rc5_bot_set_speed,		{ -BOT_SPEED_MAX, BOT_SPEED_MAX } },
+	{ RC5_CODE_JOG_R1,	rc5_bot_set_speed,		{ BOT_SPEED_MAX, BOT_SPEED_FAST } },
+	{ RC5_CODE_JOG_R2,	rc5_bot_set_speed,		{ BOT_SPEED_MAX, BOT_SPEED_NORMAL } },
+	{ RC5_CODE_JOG_R3,	rc5_bot_set_speed,		{ BOT_SPEED_MAX, BOT_SPEED_SLOW } },
+	{ RC5_CODE_JOG_R4,	rc5_bot_set_speed,		{ BOT_SPEED_MAX, BOT_SPEED_STOP } },
+	{ RC5_CODE_JOG_R5,	rc5_bot_set_speed,		{ BOT_SPEED_MAX, -BOT_SPEED_NORMAL } },
+	{ RC5_CODE_JOG_R6,	rc5_bot_set_speed,		{ BOT_SPEED_MAX, -BOT_SPEED_FAST } },
+	{ RC5_CODE_JOG_R7,	rc5_bot_set_speed,		{ BOT_SPEED_MAX, -BOT_SPEED_MAX } }
+#endif	/* JOGDIAL */
+};
+
+/*!
+ * Diese Funktion setzt die Geschwindigkeit auf den angegebenen Wert.
+ * @param par Parameter mit den zu setzenden Geschwindigkeiten.
+ */	
+static void rc5_bot_set_speed(RemCtrlFuncPar *par) {
+	if (par) {
+		target_speed_l = par->value1;
+		target_speed_r = par->value2;
+	}
+}
+
+/*!
+ * Diese Funktion aendert die Geschwindigkeit um den angegebenen Wert.
+ * @param par Parameter mit den relativen Geschwindigkeitsaenderungen.
+ */	
+static void rc5_bot_change_speed(RemCtrlFuncPar *par) {
+	if (par) {
+		target_speed_l += par->value1;
+		target_speed_r += par->value2;
+	}
+}
+
+/*!
+ * Diese Funktion setzt die zu laufenden Encoderschritte.
+ * @param par Parameter mit den zu laufenden Encoderschritten.
+ */	
+static void rc5_bot_goto(RemCtrlFuncPar *par) {
+	if (par) 	
+		bot_goto(par->value1, par->value2);
+}
 
 /*!
  * Liest ein RC5-Codeword und wertet es aus
  */
 void rc5_control(void){
-	uint16 rc5=ir_read();
-	if (rc5 !=0) {
-		RC5_Code= rc5 & RC5_MASK;		// Alle uninteressanten Bits ausblenden
-		switch(RC5_Code){
-			case RC5_CODE_PWR:	
-					// clear target Speed
-					target_speed_l=BOT_SPEED_STOP;
-					target_speed_r=BOT_SPEED_STOP;
-					// Clear goto system
-					bot_goto(0,0);
-				break;		
-
-			case RC5_CODE_UP:	
-					target_speed_l+=10;
-					target_speed_r+=10;
-				break;
-			case RC5_CODE_DOWN:	
-					target_speed_l-=10;
-					target_speed_r-=10;
-				break;
-			case RC5_CODE_LEFT:	
-					target_speed_l-=10;
-				break;
-			case RC5_CODE_RIGHT:	
-					target_speed_r-=10;
-				break;
-			
-				
-			case RC5_CODE_1:	
-					target_speed_l=BOT_SPEED_SLOW;
-					target_speed_r=BOT_SPEED_SLOW;	
-				break;					
-			case RC5_CODE_3:	
-					target_speed_l=BOT_SPEED_MAX;
-					target_speed_r=BOT_SPEED_MAX;	
-				break;
-			
-			case RC5_CODE_5: bot_goto(0,0);	break;
-			case RC5_CODE_6: bot_goto(20,-20);	break;
-			case RC5_CODE_4: bot_goto(-20,20);	break;
-			case RC5_CODE_2: bot_goto(100,100);	break;				
-			case RC5_CODE_8: bot_goto(-100,-100);	break;				
-			case RC5_CODE_7: bot_goto(-40,40);	break;
-			case RC5_CODE_9: bot_goto(40,-40);	break;
-
-			#ifdef JOGDIAL
-				case RC5_CODE_JOG_MID:
-						target_speed_l=BOT_SPEED_MAX;
-						target_speed_r=BOT_SPEED_MAX;	
-					break;
-				case RC5_CODE_JOG_L1:
-						target_speed_l=BOT_SPEED_FAST;
-						target_speed_r=BOT_SPEED_MAX;	
-				case RC5_CODE_JOG_L2:
-						target_speed_l=BOT_SPEED_NORMAL;
-						target_speed_r=BOT_SPEED_MAX;	
-					break;
-				case RC5_CODE_JOG_L3:
-						target_speed_l=BOT_SPEED_SLOW;
-						target_speed_r=BOT_SPEED_MAX;	
-					break;
-				case RC5_CODE_JOG_L4:
-						target_speed_l=BOT_SPEED_STOP;
-						target_speed_r=BOT_SPEED_MAX;	
-					break;
-					
-				case RC5_CODE_JOG_L5:
-						target_speed_l=-BOT_SPEED_NORMAL;
-						target_speed_r=BOT_SPEED_MAX;	
-					break;
-				case RC5_CODE_JOG_L6:
-						target_speed_l=-BOT_SPEED_FAST;
-						target_speed_r=BOT_SPEED_MAX;	
-					break;
-				case RC5_CODE_JOG_L7:
-						target_speed_l=-BOT_SPEED_MAX;
-						target_speed_r=BOT_SPEED_MAX;	
-					break;
-					
-				case RC5_CODE_JOG_R1:
-						target_speed_r=BOT_SPEED_FAST;
-						target_speed_l=BOT_SPEED_MAX;	
-				case RC5_CODE_JOG_R2:
-						target_speed_r=BOT_SPEED_NORMAL;
-						target_speed_l=BOT_SPEED_MAX;	
-					break;
-				case RC5_CODE_JOG_R3:
-						target_speed_r=BOT_SPEED_SLOW;
-						target_speed_l=BOT_SPEED_MAX;	
-					break;
-				case RC5_CODE_JOG_R4:
-						target_speed_r=BOT_SPEED_STOP;
-						target_speed_l=BOT_SPEED_MAX;	
-					break;
-					
-				case RC5_CODE_JOG_R5:
-						target_speed_r=-BOT_SPEED_NORMAL;
-						target_speed_l=BOT_SPEED_MAX;	
-					break;
-				case RC5_CODE_JOG_R6:
-						target_speed_r=-BOT_SPEED_FAST;
-						target_speed_l=BOT_SPEED_MAX;	
-					break;
-				case RC5_CODE_JOG_R7:
-						target_speed_r=-BOT_SPEED_MAX;
-						target_speed_l=BOT_SPEED_MAX;	
-					break;			
-			#endif
+	uint16 run;
+	uint16 rc5 = ir_read();
+	
+	if (rc5 != 0) {
+		RC5_Code= rc5 & RC5_MASK;	/* Alle uninteressanten Bits ausblenden */
+		
+		/* Suchen der auszufuehrenden Funktion */
+		for(run=0; run<sizeof(gRemCtrlAction)/sizeof(RemCtrlAction); run++) {
+			/* Funktion gefunden? */
+			if (gRemCtrlAction[run].command == RC5_Code) {
+				/* Funktion ausfuehren */
+				gRemCtrlAction[run].func(&gRemCtrlAction[run].par);
+			}
 		}
 	}
 }
