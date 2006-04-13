1ace1ea586b95c7b1a2bd4153c58e53d53fbb757
Daniel Bachfeld <dab@heise.de>
2006-04-13 12:48:56 +0000

    Kommentare f�r Variablen eingef�gt

--- motor.c
+++ motor.c
@@ -41,18 +41,18 @@ direction_t direction;		/*!< Drehrichtung der Motoren */
 #define MoMAX 255   /*!< Maximaler PWM-Wert */
 #define MoMIN 0     /*!< Minimaler PWM-Wert */
 
-volatile int8 err_r_old=0;
-volatile int8 err_r_old2=0;
-volatile int8 err_l_old=0;
-volatile int8 err_l_old2=0;
-volatile word reg_l_old=BOT_SPEED_NORMAL;
-volatile word reg_r_old=BOT_SPEED_NORMAL;
-volatile int16 last_left=BOT_SPEED_NORMAL;
-volatile int16 last_right=BOT_SPEED_NORMAL;
-volatile int16 mot_l;
-volatile int16 mot_r;
-volatile int16 tmpR=0;
-volatile int16 tmpL=0;
+volatile int8 err_r_old=0;   /*!< letzter Drehzahlfehler links */
+volatile int8 err_r_old2=0;  /*!< vorletzter Drehzahlfehler links */
+volatile int8 err_l_old=0;   /*!< letzter Drehzahlfehler rechts */
+volatile int8 err_l_old2=0;  /*!< vorletzter Drehzahlfehler rechts */
+volatile word reg_l_old=BOT_SPEED_NORMAL;  /*!< vorheriger Stellwert links*/
+volatile word reg_r_old=BOT_SPEED_NORMAL;  /*!< vorheriger Stellwert rechts*/
+volatile int16 last_left=BOT_SPEED_NORMAL; /*!< letzter Geschwindigkeitswunsch links*/ 
+volatile int16 last_right=BOT_SPEED_NORMAL;/*!< letzter Geschwindigkeitswunsch rechts*/  
+volatile int16 mot_l;       /*!< aktueller Motorstellwert links*/
+volatile int16 mot_r;       /*!< aktueller Motorstellwert rechts*/
+volatile int16 tmpR=0;      /*!< vorhergehender Wert von sensEncR */
+volatile int16 tmpL=0;      /*!< vorhergehender Wert von sensEncL */
 
 
 /*!
@@ -135,29 +135,29 @@ void motor_init(void){
 }
 
 /*!
- * @brief 	Drehzahlregelung für die Motoren des c't-Bots
+ * @brief 	Drehzahlregelung fuer die Motoren des c't-Bots
  * @author 	Daniel Bachfeld (dab@heise.de)
  * @date 	03.04.06
  * @param left	Geschwindigkeit fuer den linken Motor
  * @param right Geschwindigkeit fuer den linken Motor
- * Getrennte Drehzahlregelung für linken und rechten Motor sorgt für konstante Drehzahl und somit annähernd
- * für Geradeauslauf
+ * Getrennte Drehzahlregelung fuer linken und rechten Motor sorgt fuer konstante Drehzahl und somit annaehernd
+ * fuer Geradeauslauf
  * Feintuning von Kp, Ki, Kd verbessert die Genauigkeit und Schnelligkeit der Regelung
  * Querkopplung der Motoren verbessert Gleichlauf, beispielsweise x*(sensEncL - sensEncR) 
  * in jeden Regler einbauen
 */
 
 void speed_control (int16 left, int16 right){
-	uint8 Kp, Kd, Ki;                     // PID-Parameter
-	int16 err_l, err_r;                    // aktuelle Abweichung vom Soll-Wert 
-	int16 rmp, lmp;                       // aktueller Ist-Wert Encoderpulse
-	word reg_l, reg_r;                    //Stellwerte 
+	uint8 Kp, Kd, Ki;                 /*!< PID-Parameter */  
+	int16 err_l, err_r;               /*!<  aktuelle Abweichung vom Soll-Wert  */  
+	int16 rmp, lmp;                   /*!<  aktuelle Ist-Werte Encoderpulse */ 
+	word reg_l, reg_r;                /*!< aktuell berechnete Stellwerte */      
 	
    	  //Regler links  
       
       if ((last_left != left) || (clock_motor_control_l >1860)) {
         lmp = abs(sensEncL-tmpL);  // aktuelle Ist-Wert berechnen
-		tmpL = sensEncL;           // Anzahl der Encoderpulse merken für nächsten Aufruf merken
+		tmpL = sensEncL;           // Anzahl der Encoderpulse merken fuer naechsten Aufruf merken
         
 		if (last_left != left) {   // Bei abruptem Geschwindigkeitswechsel alte Fehler auf Null setzen
         	reg_l_old = left/9;
@@ -169,8 +169,8 @@ void speed_control (int16 left, int16 right){
 		
 		if (abs(left)==BOT_SPEED_NORMAL) {     // Zu SPEED_NORMAL gehoeren diese PID-Parameter
            Kp = 7;                        //
-           Ki = 5;                        // PID-Werte für SPEED_NORMAL 
-           Kd = 1;                        // Hier sollte man versch. Werten für Leerlauf und Last probieren
+           Ki = 5;                        // PID-Werte fuer SPEED_NORMAL 
+           Kd = 1;                        // Hier sollte man versch. Werten fuer Leerlauf und Last probieren
         }   
         
         if (abs(left)==BOT_SPEED_SLOW) {       // Zu SPEED_SLOW gehoeren diese PID-Parameter
@@ -185,11 +185,11 @@ void speed_control (int16 left, int16 right){
           reg_l = reg_l + Ki * (err_l + err_l_old)/2 ; // I-Beitrag
           reg_l = reg_l + Kd * (err_l - 2 * err_l_old + err_l_old2);  // D-Beitrag
           if (reg_l > MoMAX)
-            reg_l = MoMAX;         //berechneten Stellwert auf zulässige Groesse begrenzen
+            reg_l = MoMAX;         //berechneten Stellwert auf zulaessige Groesse begrenzen
           if (reg_l < MoMIN)
             reg_l = MoMIN;
           if (left < 0)
-            mot_l = reg_l * -1;                 // Wenn Richtung rückwärts, denn Stellwert negieren
+            mot_l = reg_l * -1;                 // Wenn Richtung rueckwaerts, denn Stellwert negieren
           else 
             mot_l = reg_l;
           err_l_old2 = err_l_old;              // alten N-2 Fehler merken
@@ -236,7 +236,7 @@ void speed_control (int16 left, int16 right){
           if (reg_r < MoMIN) 
             reg_r = MoMIN;
           if (right < 0)
-            mot_r = reg_r * -1;     // Wenn Richtung rückwärts, denn Stellwert negieren
+            mot_r = reg_r * -1;     // Wenn Richtung rueckwaerts, denn Stellwert negieren
           else 
             mot_r = reg_r;
           err_r_old2 = err_r_old;
@@ -250,7 +250,7 @@ void speed_control (int16 left, int16 right){
        last_left = left;                     // alte Geschwindigkeiten merken
        last_right = right;
        if (left==BOT_SPEED_STOP)
-         mot_l = BOT_SPEED_STOP;  // BOT_SPEED_STOP überschreibt Reglerstellwerte
+         mot_l = BOT_SPEED_STOP;  // BOT_SPEED_STOP �berschreibt Reglerstellwerte
        if (right==BOT_SPEED_STOP) 
          mot_r = BOT_SPEED_STOP;
        
