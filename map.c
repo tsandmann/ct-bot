eccb1674440545eb818b14eff7252524cbb8da22
Benjamin Benz <bbe@heise.de>
2006-11-01 14:18:05 +0000

    map theoret. auch fuer MCU geeignet

--- map.c
+++ map.c
@@ -40,10 +40,18 @@
  * ohne dass viel Platz fuer unbenutzte Felder draufgeht
  */
 
-#define MAP_RESOLUTION 	512	/*!< Aufloesung der Karte in Punkte pro Meter */
-#define MAP_SECTION_POINTS 128	/*!< Kantenlaenge einer Section in Punkten ==> eine Section braucht MAP_SECTION_POINTS*MAP_SECTION_POINTS Bytes  */
 
-#define MAP_SIZE	20	/*! Kantenlaenge der Karte in Metern. Ursprung ist der Startplatz des Bots */
+
+#ifdef MCU
+	#define MAP_SIZE			4	/*! Kantenlaenge der Karte in Metern. Ursprung ist der Startplatz des Bots */
+	#define MAP_SECTION_POINTS 32	/*!< Kantenlaenge einer Section in Punkten ==> eine Section braucht MAP_SECTION_POINTS*MAP_SECTION_POINTS Bytes  */
+	#define MAP_RESOLUTION 	(MAP_SECTION_POINTS/MAP_SIZE)	/*!< Aufloesung der Karte in Punkte pro Meter */
+#else
+	#define MAP_SIZE			20	/*! Kantenlaenge der Karte in Metern. Ursprung ist der Startplatz des Bots */
+	#define MAP_RESOLUTION 	512	/*!< Aufloesung der Karte in Punkte pro Meter */
+	#define MAP_SECTION_POINTS 128	/*!< Kantenlaenge einer Section in Punkten ==> eine Section braucht MAP_SECTION_POINTS*MAP_SECTION_POINTS Bytes  */
+#endif
+
 
 #define MAP_SECTIONS ((( MAP_SIZE*MAP_RESOLUTION)/MAP_SECTION_POINTS))
 
