99b9c26b76e6ba045c5e1495c69214a7781ccf98
Timo Sandmann <mail@timosandmann.de>
2007-03-07 18:33:50 +0000

    Reset-Counter nach dem Hinweis von Achim Pankalla wieder eingebaut (jetzt auf Reset-Info-Screen)

--- ct-Bot.c
+++ ct-Bot.c
@@ -32,6 +32,7 @@
 //	#include <avr/signal.h>
 	#include <avr/wdt.h>
 	#include "bot-2-pc.h"
+	#include <avr/eeprom.h>
 #endif
 	
 #ifdef PC
@@ -117,6 +118,8 @@ void init(void){
 				reset_flag = MCUCSR & 0x1F;	//Lese Grund fuer Reset und sichere Wert
 				MCUCSR = 0;	//setze Register auf 0x00 (loeschen)
 			#endif
+			uint8 resets = eeprom_read_byte(&resetsEEPROM) + 1;
+			eeprom_write_byte(&resetsEEPROM, resets);
 		#endif		
 	#endif
 
