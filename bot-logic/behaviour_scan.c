07be5c96de21624b99df4e5b2a9c5499ad27f6bd
Benjamin Benz <bbe@heise.de>
2007-08-06 11:21:58 +0000

    man kann nun wählen, welche Sensoren bot_scan_onthefly_behaviour auswertet

--- bot-logic/behaviour_scan.c
+++ bot-logic/behaviour_scan.c
@@ -29,26 +29,29 @@
 #include <math.h>
 
 #ifdef BEHAVIOUR_SCAN_AVAILABLE
+
+#ifndef MAP_AVAILABLE
+	#error MAP_AVAILABLE muss an sein, damit behaviour_scan.c etwas sinnvolles tun kann
+#endif
+
+uint8 scan_on_the_fly_source = SENSOR_LOCATION || SENSOR_DISTANCE; 
+
 /*!
- * Der Roboter faehrt einen Vollkreis und scannt dabei die Umgebung
+ * Der Roboter aktualisiert kontinuierlich seine Karte
  * @param *data der Verhaltensdatensatz
  */
-void bot_scan_onthefly_behaviour(Behaviour_t *data){	
-	#define ONTHEFLY_DIST_RESOLUTION 0.02		/*!< Alle wieviel gefahrene Strecke [m] soll die Karte aktualisiert werden. Achtung er prueft x und y getrennt, daher ist die tatsaechlich zurueckgelegte Strecke im worst case sqrt(2)*ONTHEFLY_DIST_RESOLUTION  */
-	#define ONTHEFLY_ANGLE_RESOLUTION 10		/*!< Alle wieviel Gerad Drehung [m] soll die Karte aktualisiert werden */
-	
-
-	
-	#ifdef MAP_AVAILABLE
+void bot_scan_onthefly_behaviour(Behaviour_t *data){		
 		static float last_x, last_y, last_head;
 		
 		float diff_x = fabs(x_pos-last_x);
 		float diff_y = fabs(y_pos-last_y);
 
 		// Wenn der Bot faehrt, aktualisieren wir alles
-		if ((diff_x > ONTHEFLY_DIST_RESOLUTION) ||( diff_y > ONTHEFLY_DIST_RESOLUTION)){
-			update_map_location(x_pos,y_pos);
-			update_map(x_pos,y_pos,heading,sensDistL,sensDistR);
+		if ((diff_x > SCAN_ONTHEFLY_DIST_RESOLUTION) ||( diff_y > SCAN_ONTHEFLY_DIST_RESOLUTION)){
+			if ((scan_on_the_fly_source && SENSOR_LOCATION) != 0)
+				update_map_location(x_pos,y_pos);
+			if ((scan_on_the_fly_source && SENSOR_DISTANCE) != 0)
+				update_map(x_pos,y_pos,heading,sensDistL,sensDistR);
 
 			last_x=x_pos;
 			last_y=y_pos;
@@ -58,7 +61,7 @@ void bot_scan_onthefly_behaviour(Behaviour_t *data){
 	
 		float diff_head = fabs(last_head-heading);		
 		// Wenn der bot nur dreht, aktualisieren wir nur die Blickstrahlen
-		if ( diff_head > ONTHEFLY_ANGLE_RESOLUTION ){
+		if (( diff_head > SCAN_ONTHEFLY_ANGLE_RESOLUTION) && ((scan_on_the_fly_source && SENSOR_DISTANCE) != 0)  ){
 			update_map(x_pos,y_pos,heading,sensDistL,sensDistR);
 			last_head=heading;
 		}
@@ -69,7 +72,6 @@ void bot_scan_onthefly_behaviour(Behaviour_t *data){
 				print_map();
 			}
 		*/	
-	#endif  //MAP_AVAILABLE
 }
 
 
@@ -142,46 +144,44 @@ void bot_scan(Behaviour_t* caller){
 //	print_map();	
 }
 
-#ifdef MAP_AVAILABLE
-	/*! 
-	 * eigentliche Aufrufroutine zum Eintragen des Abgrundes in den Mapkoordinaten, wenn
-	 * die Abgrundsensoren zugeschlagen haben  
-	 * @param x aktuelle Bot-Koordinate als Welt- (nicht Map-) Koordinaten
-	 * @param y aktuelle Bot-Koordinate als Welt- (nicht Map-) Koordinaten
-	 * @param head Blickrichtung 
-	 */ 
-	void update_map_hole(float x, float y, float head) {
-		float h= head * M_PI /180;  // Umrechnung in Bogenmass 
-		// uint8 border_behaviour_fired=False;
+/*! 
+ * eigentliche Aufrufroutine zum Eintragen des Abgrundes in den Mapkoordinaten, wenn
+ * die Abgrundsensoren zugeschlagen haben  
+ * @param x aktuelle Bot-Koordinate als Welt- (nicht Map-) Koordinaten
+ * @param y aktuelle Bot-Koordinate als Welt- (nicht Map-) Koordinaten
+ * @param head Blickrichtung 
+ */ 
+void update_map_hole(float x, float y, float head) {
+	float h= head * M_PI /180;  // Umrechnung in Bogenmass 
+	// uint8 border_behaviour_fired=False;
 
-		if (sensBorderL > BORDER_DANGEROUS) {
-			//Ort des linken Sensors in Weltkoordinaten (Mittelpunktentfernung)
-			float Pl_x = x - (DISTSENSOR_POS_B_SW * sin(h));
-			float Pl_y = y + (DISTSENSOR_POS_B_SW * cos(h));
-			update_map_sensor_hole(Pl_x,Pl_y,h); // Eintragen des Loches in die Map	   
-		}
-		
-		if (sensBorderR > BORDER_DANGEROUS) {        
-			//Ort des rechten Sensors in Weltkoordinaten (Mittelpunktentfernung)
-			float Pr_x = x + (DISTSENSOR_POS_B_SW * sin(h));
-			float Pr_y = y - (DISTSENSOR_POS_B_SW * cos(h));
-			update_map_sensor_hole(Pr_x,Pr_y,h); // Eintragen des Loches in die Map
-		}
+	if (sensBorderL > BORDER_DANGEROUS) {
+		//Ort des linken Sensors in Weltkoordinaten (Mittelpunktentfernung)
+		float Pl_x = x - (DISTSENSOR_POS_B_SW * sin(h));
+		float Pl_y = y + (DISTSENSOR_POS_B_SW * cos(h));
+		update_map_sensor_hole(Pl_x,Pl_y,h); // Eintragen des Loches in die Map	   
 	}
-
-	/*!
-	 * Notfallhandler, ausgefuehrt bei Abgrunderkennung; muss registriert werden um
-	 * den erkannten Abgrund in die Map einzutragen
-	 */
-	void border_in_map_handler(void) {
-		// Routine muss zuerst checken, ob on_the_fly auch gerade aktiv ist, da nur in diesem
-		// Fall etwas gemacht werden muss
-		if (!behaviour_is_activated(bot_scan_onthefly_behaviour)) 
-			return;
-
-		/* bei Abgrunderkennung Position des Abgrundes in Map mit -128 eintragen */
-		update_map_hole(x_pos,y_pos,heading);
+	
+	if (sensBorderR > BORDER_DANGEROUS) {        
+		//Ort des rechten Sensors in Weltkoordinaten (Mittelpunktentfernung)
+		float Pr_x = x + (DISTSENSOR_POS_B_SW * sin(h));
+		float Pr_y = y - (DISTSENSOR_POS_B_SW * cos(h));
+		update_map_sensor_hole(Pr_x,Pr_y,h); // Eintragen des Loches in die Map
 	}
-#endif	// MAP_AVAILABLE
+}
+
+/*!
+ * Notfallhandler, ausgefuehrt bei Abgrunderkennung; muss registriert werden um
+ * den erkannten Abgrund in die Map einzutragen
+ */
+void border_in_map_handler(void) {
+	// Routine muss zuerst checken, ob on_the_fly auch gerade aktiv ist, da nur in diesem
+	// Fall etwas gemacht werden muss
+	if (!behaviour_is_activated(bot_scan_onthefly_behaviour)) 
+		return;
+
+	/* bei Abgrunderkennung Position des Abgrundes in Map mit -128 eintragen */
+	update_map_hole(x_pos,y_pos,heading);
+}
 
 #endif	// BEHAVIOUR_SCAN_AVAILABLE
