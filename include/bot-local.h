e63cd1ef4e14fcf28cae626faefb5f38fb9a62c5
Timo Sandmann <mail@timosandmann.de>
2016-05-22 16:35:21 +0200

    dependency for EXPANSION_BOARD_MOD_AVAILABLE added, cleanup of config settings

--- include/bot-local.h
+++ include/bot-local.h
@@ -97,14 +97,9 @@
 /* System-Konstanten
    --> Diese Aenderungen sind mit lokalen Hardware-Anpassungen verbunden! */
 
-#define F_CPU	16000000UL	/**< CPU-Frequenz [Hz] */
-//#define F_CPU	20000000UL	/**< CPU-Frequenz [Hz] */
+#define F_CPU	16000000UL	/**< CPU-Frequenz [Hz] (16000000UL oder 20000000UL) */
 
-//#define UART_BAUD	57600	/**< Baudrate  57600 fuer UART-Kommunikation */
-#define UART_BAUD	115200	/**< Baudrate 115200 fuer UART-Kommunikation */
-//#define UART_BAUD	230400	/**< Baudrate 230400 fuer UART-Kommunikation */
-//#define UART_BAUD	460800	/**< Baudrate 460800 fuer UART-Kommunikation */
-//#define UART_BAUD	500000	/**< Baudrate 500k fuer UART-Kommunikation */
+#define UART_BAUD	115200	/**< Baudrate fuer UART-Kommunikation (moegliche Werte sind 57600, 115200, 230400, 500000) */
 
 #define UART_LINUX_PORT		"/dev/ttyAMA0"	/**< UART Port vom ARM-Linux-Board fuer Verbinung zum ATmega */
 #define BOT_RESET_GPIO		"/sys/class/gpio/gpio17/value"	/**< Pfad zum Reset-GPIO vom ARM-Linux-Board */
@@ -158,6 +153,10 @@
 /************************************************************
  * Some Dependencies!!!
  ************************************************************/
+
+#ifdef PC
+#undef EXPANSION_BOARD_MOD_AVAILABLE
+#endif
 
 #ifdef EXPANSION_BOARD_MOD_AVAILABLE
 #undef EXPANSION_BOARD_AVAILABLE	// deaktiviert EXPANSION_BOARD_AVAILABLE
