74633e24af572abc6b193ec8ff3ad6d4a10cb9e3
Timo Sandmann <mail@timosandmann.de>
2017-02-26 01:46:57 +0100

    travis build test settings updated

--- tests/run_tests_mcu.sh
+++ tests/run_tests_mcu.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-for filename in tests/mcu/*.h; do
-	cp -v $filename bot-local-override.h
+export MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [[ "$TRAVIS" == "true" ]]; then
+	source $MYDIR/avr_toolchain_install.sh
+fi
+
+cd $MYDIR/../
+for filename in $MYDIR/mcu/*.h; do
+	cp -v $filename $MYDIR/../bot-local-override.h
 	cores=$(grep -c "^processor" /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
 	echo "using $cores parallel jobs"
 	make DEVICE=MCU WERROR=1 WCONVERSION=0 -j$cores
 	rc=$?
-	rm bot-local-override.h
+	rm $MYDIR/../bot-local-override.h
 	if [[ $rc != 0 ]]; then
 		echo ""; echo ""; echo "TEST $filename FAILED."; echo ""; echo ""
 		make DEVICE=MCU clean >/dev/null
-		exit $rc; 
+		exit $rc;
 	fi
 	make DEVICE=MCU clean >/dev/null
 done
