1d2f72dd5f349e613657e0937f4dca03ee204169
Timo Sandmann <mail@timosandmann.de>
2017-07-22 18:37:06 +0200

    bugfix build test script for rpi2 and rpi3

--- tests/run_tests_rpi3.sh
+++ tests/run_tests_rpi3.sh
@@ -5,15 +5,17 @@ export MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [[ "`uname`" == "Darwin" ]]; then
 	if [[ ! -e $MYDIR/armv8l-unknown-linux-gnueabihf ]]; then
-		command -v armv8l-linux-gnueabihf-g++ >/dev/null 2>&1 || { git clone --depth=1 --branch=master https://github.com/tsandmann/armv8l-toolchain-mac.git $MYDIR/armv8l-unknown-linux-gnueabihf }
+		command -v armv8l-linux-gnueabihf-g++ >/dev/null 2>&1 || { git clone --depth=1 --branch=master https://github.com/tsandmann/armv8l-toolchain-mac.git $MYDIR/armv8l-unknown-linux-gnueabihf; }
 	fi
+
 	export PATH=$MYDIR/armv8l-unknown-linux-gnueabihf/bin:$PATH
 fi
 
 if [[ "`uname`" == "Linux" ]]; then
 	if [[ ! -e $MYDIR/armv8l-unknown-linux-gnueabihf ]]; then
-		command -v armv8l-linux-gnueabihf-g++ >/dev/null 2>&1 || { git clone --depth=1 --branch=master https://github.com/tsandmann/armv8l-toolchain-linux.git $MYDIR/armv8l-unknown-linux-gnueabihf }
+		command -v armv8l-linux-gnueabihf-g++ >/dev/null 2>&1 || { git clone --depth=1 --branch=master https://github.com/tsandmann/armv8l-toolchain-linux.git $MYDIR/armv8l-unknown-linux-gnueabihf; }
 	fi
+
 	export PATH=$MYDIR/armv8l-unknown-linux-gnueabihf/bin:$PATH
 fi
 
