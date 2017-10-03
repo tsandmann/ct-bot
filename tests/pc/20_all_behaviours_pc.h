9f2d2a2ec456e7cb8aa2a016ee910d76b2c15f0c
Timo Sandmann <mail@timosandmann.de>
2017-10-03 18:57:59 +0200

    test cases updated

--- tests/pc/20_all_behaviours_pc.h
+++ tests/pc/20_all_behaviours_pc.h
@@ -34,8 +34,8 @@
 
 /* Display-Funktionen */
 #define DISPLAY_AVAILABLE					/**< Display-Funktionen aktiv */
-#define KEYPAD_AVAILABLE					/**< Keypad-Eingabe vorhanden? */
-#define DISPLAY_REMOTE_AVAILABLE			/**< Sende LCD Anzeigedaten an den Simulator */
+#define KEYPAD_AVAILABLE						/**< Keypad-Eingabe vorhanden? */
+#define DISPLAY_REMOTE_AVAILABLE				/**< Sende LCD Anzeigedaten an den Simulator */
 #define WELCOME_AVAILABLE					/**< kleiner Willkommensgruss */
 
 /* Sensorauswertung */
@@ -57,63 +57,63 @@
 #define SHIFT_AVAILABLE						/**< Shift Register */
 #define ENA_AVAILABLE						/**< Enable-Leitungen */
 #define LED_AVAILABLE						/**< LEDs aktiv */
-#define IR_AVAILABLE						/**< Infrarot Fernbedienung aktiv */
+#define IR_AVAILABLE							/**< Infrarot Fernbedienung aktiv */
 #define RC5_AVAILABLE						/**< Key-Mapping fuer IR-RC aktiv */
 
 /* Sonstiges */
 #define BEHAVIOUR_AVAILABLE					/**< Nur wenn dieser Parameter gesetzt ist, exisitiert das Verhaltenssystem */
 #define POS_STORE_AVAILABLE					/**< Positionsspeicher vorhanden */
-#define OS_AVAILABLE						/**< Aktiviert BotOS fuer Threads und Scheduling */
+#define OS_AVAILABLE							/**< Aktiviert BotOS fuer Threads und Scheduling */
 #define ARM_LINUX_BOARD						/**< Code fuer ARM-Linux Board aktivieren, wenn ein ARM-Linux-* Target ausgewaehlt wurde. Fuehrt den high-level Code und die Verhalten aus */
 
 /* Demo-Verhalten */
 #define BEHAVIOUR_SIMPLE_AVAILABLE				/**< Beispielverhalten */
-#define BEHAVIOUR_DRIVE_SQUARE_AVAILABLE 			/**< Demoverhalten im Quadrat fahren */
+#define BEHAVIOUR_DRIVE_SQUARE_AVAILABLE 		/**< Demoverhalten im Quadrat fahren */
 
 /* Notfall-Verhalten */
-#define BEHAVIOUR_AVOID_BORDER_AVAILABLE 			/**< Abgruenden ausweichen */
+#define BEHAVIOUR_AVOID_BORDER_AVAILABLE 		/**< Abgruenden ausweichen */
 #define BEHAVIOUR_AVOID_COL_AVAILABLE 			/**< Hindernis ausweichen */
 #define BEHAVIOUR_HANG_ON_AVAILABLE 				/**< Erkennen des Haengenbleibens als Notfallverhalten */
 
 /* Positionierungs-Verhalten */
-#define BEHAVIOUR_TURN_AVAILABLE 					/**< Dreh Verhalten */
+#define BEHAVIOUR_TURN_AVAILABLE 				/**< Dreh Verhalten */
 #define BEHAVIOUR_TURN_TEST_AVAILABLE 			/**< Test des Dreh Verhaltens */
-#define BEHAVIOUR_GOTO_POS_AVAILABLE 				/**< Position anfahren */
+#define BEHAVIOUR_GOTO_POS_AVAILABLE 			/**< Position anfahren */
 #define BEHAVIOUR_GOTO_OBSTACLE_AVAILABLE 		/**< Abstand zu Hindernis einhalten */
 #define BEHAVIOUR_DRIVE_STACK_AVAILABLE 			/**< Abfahren der auf dem Stack gesicherten Koordinaten */
-#define BEHAVIOUR_TEST_ENCODER_AVAILABLE 			/**< Encoder-Test Verhalten */
+#define BEHAVIOUR_TEST_ENCODER_AVAILABLE 		/**< Encoder-Test Verhalten */
 
 /* Anwendungs-Verhalten */
-#define BEHAVIOUR_SOLVE_MAZE_AVAILABLE 				/**< Wandfolger */
+#define BEHAVIOUR_SOLVE_MAZE_AVAILABLE 			/**< Wandfolger */
 #define BEHAVIOUR_FOLLOW_LINE_AVAILABLE			/**< Linienfolger */
 #define BEHAVIOUR_FOLLOW_LINE_ENHANCED_AVAILABLE 	/**< erweiterter Linienfolger, der auch mit Unterbrechungen und Hindernissen klarkommt */
 #define BEHAVIOUR_PATHPLANING_AVAILABLE 			/**< Pfadplanungsverhalten */
 #define BEHAVIOUR_OLYMPIC_AVAILABLE				/**< Olympiadenverhalten */
-#define BEHAVIOUR_CATCH_PILLAR_AVAILABLE 			/**< Suche eine Dose und fange sie ein */
-#define BEHAVIOUR_CLASSIFY_OBJECTS_AVAILABLE 		/**< Trennt zwei Arten von Dosen (hell / dunkel) */
-#define BEHAVIOUR_TRANSPORT_PILLAR_AVAILABLE 		/**< Transport-Pillar Verhalten */
+#define BEHAVIOUR_CATCH_PILLAR_AVAILABLE 		/**< Suche eine Dose und fange sie ein */
+#define BEHAVIOUR_CLASSIFY_OBJECTS_AVAILABLE 	/**< Trennt zwei Arten von Dosen (hell / dunkel) */
+#define BEHAVIOUR_TRANSPORT_PILLAR_AVAILABLE 	/**< Transport-Pillar Verhalten */
 #define BEHAVIOUR_FOLLOW_OBJECT_AVAILABLE 		/**< verfolge ein (bewegliches) Objekt */
 #define BEHAVIOUR_FOLLOW_WALL_AVAILABLE 			/**< Follow Wall Explorer Verhalten */
 #define BEHAVIOUR_DRIVE_AREA_AVAILABLE 			/**< flaechendeckendes Fahren mit Map */
 #define BEHAVIOUR_LINE_SHORTEST_WAY_AVAILABLE 	/**< Linienfolger ueber Kreuzungen zum Ziel */
 #define BEHAVIOUR_DRIVE_CHESS_AVAILABLE 			/**< Schach fuer den Bot */
-#define BEHAVIOUR_SCAN_BEACONS_AVAILABLE 			/**< Suchen von Landmarken zur Lokalisierung */
+#define BEHAVIOUR_SCAN_BEACONS_AVAILABLE 		/**< Suchen von Landmarken zur Lokalisierung */
 #define BEHAVIOUR_UBASIC_AVAILABLE 				/**< uBasic Verhalten */
 #define BEHAVIOUR_ABL_AVAILABLE 					/**< ABL-Interpreter */
 #define BEHAVIOUR_NEURALNET_AVAILABLE 			/**< neuronales Netzwerk */
 #define BEHAVIOUR_DRIVE_NEURALNET_AVAILABLE 		/**< Fahrverhalten fuer das neuronale Netzwerk */
 
 /* Kalibrierungs-Verhalten */
-#define BEHAVIOUR_CALIBRATE_PID_AVAILABLE			/**< Kalibrierungsverhalten fuer Motorregelung */
-#define BEHAVIOUR_CALIBRATE_SHARPS_AVAILABLE 		/**< Kalibrierungsverhalten fuer Distanzsensoren */
+#define BEHAVIOUR_CALIBRATE_PID_AVAILABLE		/**< Kalibrierungsverhalten fuer Motorregelung */
+#define BEHAVIOUR_CALIBRATE_SHARPS_AVAILABLE 	/**< Kalibrierungsverhalten fuer Distanzsensoren */
 
 /* System-Verhalten */
-#define BEHAVIOUR_SCAN_AVAILABLE 					/**< Gegend scannen */
-#define BEHAVIOUR_SERVO_AVAILABLE 					/**< Kontrollverhalten fuer die Servos */
-#define BEHAVIOUR_REMOTECALL_AVAILABLE 				/**< Remote-Kommandos */
-#define BEHAVIOUR_MEASURE_DISTANCE_AVAILABLE 		/**< Distanzesensorasuwertung */
-#define BEHAVIOUR_DELAY_AVAILABLE 					/**< Delay-Routine als Verhalten */
-#define BEHAVIOUR_CANCEL_BEHAVIOUR_AVAILABLE 		/**< Deaktivieren von Verhalten, wenn eine Abbruchbedingung erfuellt ist */
+#define BEHAVIOUR_SCAN_AVAILABLE 				/**< Gegend scannen */
+#define BEHAVIOUR_SERVO_AVAILABLE 				/**< Kontrollverhalten fuer die Servos */
+#define BEHAVIOUR_REMOTECALL_AVAILABLE 			/**< Remote-Kommandos */
+#define BEHAVIOUR_MEASURE_DISTANCE_AVAILABLE 	/**< Distanzesensorasuwertung */
+#define BEHAVIOUR_DELAY_AVAILABLE 				/**< Delay-Routine als Verhalten */
+#define BEHAVIOUR_CANCEL_BEHAVIOUR_AVAILABLE 	/**< Deaktivieren von Verhalten, wenn eine Abbruchbedingung erfuellt ist */
 #define BEHAVIOUR_GET_UTILIZATION_AVAILABLE		/**< CPU-Auslastung eines Verhaltens messen */
 #define BEHAVIOUR_HW_TEST_AVAILABLE 				/**< Testverhalten (ehemals TEST_AVAILABLE_ANALOG, _DIGITAL, _MOTOR) */
 
