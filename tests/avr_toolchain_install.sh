6a842f7c4e4df82496380dac3e8a195480454b5c
Timo Sandmann <mail@timosandmann.de>
2018-01-14 18:11:54 +0100

    avr toolchain for travis build tests updated

--- tests/avr_toolchain_install.sh
+++ tests/avr_toolchain_install.sh
@@ -5,7 +5,7 @@ export MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [[ "`uname`" == "Darwin" ]]; then
 	if [[ ! -e $MYDIR/avr8-gnu-toolchain-darwin_x86_64 ]]; then
 		cd $MYDIR/
-		curl -L -o avr8-gnu-toolchain-osx.tar.gz 'http://distribute.atmel.no/tools/opensource/Atmel-AVR-GNU-Toolchain/3.6.0/avr8-gnu-toolchain-osx-3.6.0.487-darwin.any.x86_64.tar.gz'
+		curl -L -o avr8-gnu-toolchain-osx.tar.gz 'http://distribute.atmel.no/tools/opensource/Atmel-AVR-GNU-Toolchain/3.6.1/avr8-gnu-toolchain-osx-3.6.1.495-darwin.any.x86_64.tar.gz'
 		tar xzf avr8-gnu-toolchain-osx.tar.gz
 	fi
 
@@ -15,7 +15,7 @@ fi
 if [[ "`uname`" == "Linux" ]]; then
 	if [[ ! -e $MYDIR/avr8-gnu-toolchain-linux_x86_64 ]]; then
 		cd $MYDIR/
-		curl -L -o avr8-gnu-toolchain-linux.tar.gz 'http://www.atmel.com/images/avr8-gnu-toolchain-3.5.4.1709-linux.any.x86_64.tar.gz'
+		curl -L -o avr8-gnu-toolchain-linux.tar.gz 'http://ww1.microchip.com/downloads/en/DeviceDoc/avr8-gnu-toolchain-3.5.4.1709-linux.any.x86_64.tar.gz'
 		tar xzf avr8-gnu-toolchain-linux.tar.gz
 	fi
 
