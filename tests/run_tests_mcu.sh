1376f704c05ec59c71b3d97db4d445cdefda3983
Timo Sandmann <mail@timosandmann.de>
2020-03-20 20:52:04 +0100

    bugfix for updated CI jobs

--- tests/run_tests_mcu.sh
+++ tests/run_tests_mcu.sh
@@ -6,7 +6,7 @@ if [[ "$TRAVIS" == "true" ]]; then
 
 if [[ "`uname`" == "Darwin" ]]; then
 	if [[ ! -e $MYDIR/avr8-gnu-toolchain-darwin_x86_64 ]]; then
-		cd $MYDIR/; curl -L -o avr8-gnu-toolchain-osx.tar.gz 'https://dl.bintray.com/platformio/dl-packages/toolchain-atmelavr-darwin_x86_64-1.70300.191015.tar.gz'; tar xzf avr8-gnu-toolchain-osx.tar.gz
+		mkdir $MYDIR/avr8-gnu-toolchain-darwin_x86_64; cd $MYDIR/avr8-gnu-toolchain-darwin_x86_64; curl -L -o avr8-gnu-toolchain-osx.tar.gz 'https://dl.bintray.com/platformio/dl-packages/toolchain-atmelavr-darwin_x86_64-1.70300.191015.tar.gz'; tar xzf avr8-gnu-toolchain-osx.tar.gz
 	fi
 
 	export PATH=$MYDIR/avr8-gnu-toolchain-darwin_x86_64/bin:$PATH
@@ -14,7 +14,7 @@ fi
 
 if [[ "`uname`" == "Linux" ]]; then
 	if [[ ! -e $MYDIR/avr8-gnu-toolchain-linux_x86_64 ]]; then
-		cd $MYDIR/; curl -L -o avr8-gnu-toolchain-linux.tar.gz 'https://dl.bintray.com/platformio/dl-packages/toolchain-atmelavr-linux_x86_64-1.70300.191015.tar.gz'; tar xzf avr8-gnu-toolchain-linux.tar.gz
+		mkdir $MYDIR/avr8-gnu-toolchain-linux_x86_64; cd $MYDIR/avr8-gnu-toolchain-linux_x86_64; curl -L -o avr8-gnu-toolchain-linux.tar.gz 'https://dl.bintray.com/platformio/dl-packages/toolchain-atmelavr-linux_x86_64-1.70300.191015.tar.gz'; tar xzf avr8-gnu-toolchain-linux.tar.gz
 	fi
 
 	export PATH=$MYDIR/avr8-gnu-toolchain-linux_x86_64/bin:$PATH
