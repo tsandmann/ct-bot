cd62a0fa0a1dc04ebf22c8269477509f72b3c7eb
Timo Sandmann <mail@timosandmann.de>
2020-03-21 21:41:49 +0100

    i2c driver improved
    spec conform handling of stop-condition in case of i2c_wait()

--- include/i2c.h
+++ include/i2c.h
@@ -20,13 +20,15 @@
 /**
  * \file 	i2c.h
  * \brief 	I2C-Treiber, derzeit nur Master, interruptbasiert
- * \author 	Timo Sandmann (mail@timosandmann.de)
+ * \author 	Timo Sandmann
  * \date 	05.09.2007
  */
 
 
 #ifndef I2C_H_
 #define I2C_H_
+
+#include "ct-Bot.h"
 
 #ifdef I2C_AVAILABLE
 #include <stddef.h>
@@ -45,7 +47,7 @@ void i2c_init(uint8_t bitrate);
  * \param *pRx	Zeiger auf Puffer fuer zu lesende Daten
  * \param nRx	Anzahl der zu lesenden Bytes
  */
-void i2c_write_read(uint8_t sla, void * pTx, uint8_t nTx, void * pRx, uint8_t nRx);
+void i2c_write_read(uint8_t sla, const void* pTx, uint8_t nTx, void* pRx, uint8_t nRx);
 
 /**
  * Sendet ein Byte an einen I2C-Slave und liest anschliessend nRx Bytes
@@ -54,7 +56,7 @@ void i2c_write_read(uint8_t sla, void * pTx, uint8_t nTx, void * pRx, uint8_t nR
  * \param *pRx		Zeiger auf Puffer fuer zu lesende Daten
  * \param nRx		Anzahl der zu lesenden Bytes
  */
-void i2c_read(uint8_t sla, uint8_t txData, void * pRx, uint8_t nRx);
+void i2c_read(uint8_t sla, uint8_t txData, void* pRx, uint8_t nRx);
 
 /**
  * Sendet nTx Bytes an einen I2C-Slave
@@ -62,7 +64,7 @@ void i2c_read(uint8_t sla, uint8_t txData, void * pRx, uint8_t nRx);
  * \param *pTx	Zeiger auf Puffer fuer zu sendende Daten
  * \param nTx	Anzahl der zu sendenden Bytes
  */
-static inline void i2c_write(uint8_t sla, void * pTx, uint8_t nTx) {
+static inline void i2c_write(uint8_t sla, const void* pTx, uint8_t nTx) {
 	i2c_write_read(sla, pTx, nTx, NULL, 0);
 }
 
@@ -71,6 +73,10 @@ static inline void i2c_write(uint8_t sla, void * pTx, uint8_t nTx) {
  * \return TW_NO_INFO (0xf8) falls alles ok, sonst Fehlercode
  */
 uint8_t i2c_wait(void);
+
+static inline void i2c_off(void) {
+	TWCR = 0; // I2C aus
+}
 
 #endif // I2C_AVAILABLE
 #endif // I2C_H_
